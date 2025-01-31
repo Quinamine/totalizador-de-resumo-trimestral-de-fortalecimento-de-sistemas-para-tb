f50625aa3a0016fb2131dc05786221ba1873d6d3
Adérito Félix Quinamine <quinamine.aderitofelix@gmail.com>
2025-01-31 05:16:27 +0200

    JS: Actualizei a invocação da função 'desejar festas felizes' para dentro de um 'if(mes===dezembro)'.

--- fisiologia/festasfelizes.js
+++ fisiologia/festasfelizes.js
@@ -3,19 +3,16 @@ const autoTxt = "Votos de Festas Felizes e um Próspero Ano Novo!";
 let txtIndex = 0;
 let txtLength = autoTxt.length;
 const output = document.querySelector(".topo-propaganda__paragrafo");
-function autoDigitacao() {
+function desejarFestasFelizes() {
     if(txtIndex < txtLength) {
         output.textContent += autoTxt[txtIndex];
         txtIndex++;
-        setTimeout(autoDigitacao, 150);
+        setTimeout(desejarFestasFelizes, 150);
     }
 }
-let variavelDeSetTime = setTimeout(autoDigitacao, 150);
-window.addEventListener("load", () => {
-    const tempo = new Date();
-    let mesIndex = tempo.getMonth();
-    if(mesIndex !== 11) {
-        document.querySelector(".topo-propaganda--festas-felizes").hidden = true;
-        clearInterval(variavelDeSetTime);
-    }
-});
+const tempo = new Date();
+let mesIndex = tempo.getMonth();
+if(mesIndex === 11) {
+    desejarFestasFelizes();
+    document.querySelector(".topo-propaganda--festas-felizes").classList.remove("topo-propaganda--off");
+} 
