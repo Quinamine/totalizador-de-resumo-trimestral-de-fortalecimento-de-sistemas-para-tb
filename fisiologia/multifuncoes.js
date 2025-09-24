9109e6de46b4c1c366815d99a63ce5981a80832d
Adérito Félix Quinamine <quinamine.aderitofelix@gmail.com>
2025-09-24 19:19:58 +0200

    HTML/JS: Actualizei o conteúdo 'Sobre' e a função do ano de copyright.

--- fisiologia/multifuncoes.js
+++ fisiologia/multifuncoes.js
@@ -69,7 +69,7 @@ const aqd = {
 function actualizarAnoDeCopyright() {
     const tempo = new Date();
     let anoActual = tempo.getFullYear();
-    if(anoActual < 2024) anoActual = 2024;
+    if(anoActual < 2025) anoActual = 2025;
     const currentYearOutput = document.querySelector(".footer__current-year");
     currentYearOutput.textContent = anoActual;
 }
